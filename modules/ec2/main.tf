25ace1955a81a85a6f23403ca54cc7dd176b1fcd
sophie-in-the-cloud <dlagytjs20@gmail.com>
2025-09-17 09:34:24 +0900

    add qdev instance

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -24,3 +24,14 @@ resource "aws_instance" "management" {
     Name = "${var.prefix}-management"
   }
 }
+
+resource "aws_instance" "qdev" {
+  ami                    = var.management_ami
+  instance_type          = var.management_instance_type
+  subnet_id              = var.private_subnet_id
+  vpc_security_group_ids = [var.management_sg_id]
+  key_name               = aws_key_pair.bastion_key.key_name
+  tags = {
+    Name = "${var.prefix}-qdev"
+  }
+}
